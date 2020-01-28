65d0f6a771d3364aa162fdb11dacfbd923739519
Noland Seigler <nolandseigler@gmail.com>
2020-01-28 12:22:07 -0600

    loops complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,36 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.printf(" %d ", i);
+//            i++;
+//        }
+//        int j = 100;
+//        do {
+//            System.out.printf("%d \n", j);
+//            j -= 5;
+//        } while(j >= -10);
+//        long k = 2;
+//        do {
+//            System.out.printf("%d \n", k);
+//            k *= k;
+//        } while (k < 1000000);
+//        double k = 2;
+//        do {
+//            System.out.printf("%.0f \n", k);
+//            k = Math.pow(k, 2);
+//        } while (k < 1000000);
+        for (int i = 5; i <= 15; i++) {
+            System.out.printf(" %d ", i);
+        };
+        System.out.println('\n');
+        for (int j = 100; j >= -10; j -= 5) {
+            System.out.printf("%d \n", j);
+        };
+        System.out.print('\n');
+        for (long k = 2; k < 1000000; k *= k) {
+            System.out.printf("%d \n", k);
+        }
 
     }
 }
