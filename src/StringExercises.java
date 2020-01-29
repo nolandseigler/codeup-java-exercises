8494c8b184396e01b85abfbbf28cf5e6a52596ad
Noland Seigler <nolandseigler@gmail.com>
2020-01-29 11:00:06 -0600

    first part of StringExercises is complete

---
+++ src/StringExercises.java
@@ -0,0 +1,9 @@
+public class StringExercises {
+    public static void main(String[] args) {
+        String lyrics = "We don't need no education \nWe don't need no thought control";
+        String checkout = "Check \"this\" out!, \"s inside of \"s!";
+        String windows = "In windows, the main drive is usually C:\\";
+        String slashes = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\";
+        System.out.printf("%s\n%s\n%s\n%s\n", lyrics, checkout, windows, slashes);
+    };
+};
