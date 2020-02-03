dc23349b100c5507be28bd21614131cf4efe10c9
Noland Seigler <nolandseigler@gmail.com>
2020-02-03 11:37:34 -0600

    on exrcise three. getters and setters made

---
+++ src/movies/Movie.java
@@ -0,0 +1,23 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    public String getName () {
+        return this.name;
+    }
+    public void setName (String name) {
+        this.name = name;
+    }
+    public String getCategory () {
+        return this.category;
+    }
+    public void setCategory (String category) {
+        this.category = category;
+    }
+    Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+}
