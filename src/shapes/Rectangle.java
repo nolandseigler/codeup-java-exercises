53836d10b282956c7b46e64c0fa1866790323606
Noland Seigler <nolandseigler@gmail.com>
2020-02-04 11:38:40 -0600

    Rectangle created

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Rectangle {
+    protected int length;
+    protected int width;
+    Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+
+    }
+    int getPerimeter() {
+        return 2 * this.length + 2 * width;
+    }
+    int getArea() {
+        return this.length * this.width;
+    }
+}
