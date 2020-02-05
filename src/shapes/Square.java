0ddca9c8b9ef182a4b265b0714cfe8a34d395117
Noland Seigler <nolandseigler@gmail.com>
2020-02-05 10:58:39 -0600

    interfaces exercise started

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,16 +2,4 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    Square(int side) {
-        super(side, side);
-    }
-    void getLengthAndWidth() {
-        System.out.println("" + this.length + " " + this.width);
-    }
-    int getPerimeter() {
-        return 4 * this.length;
-    }
-    int getArea() {
-        return this.length * this.length;
-    }
 }
