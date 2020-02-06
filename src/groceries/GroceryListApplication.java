b154a5f767be53b9b00ef18c3aa63baf3b0fa9c2
Noland Seigler <nolandseigler@gmail.com>
2020-02-06 13:20:59 -0600

    made groceries package with GroceryListApplication

---
+++ src/groceries/GroceryListApplication.java
@@ -0,0 +1,11 @@
+package groceries;
+
+import util.Input;
+
+class GroceryListApplication {
+    private static Input myInput = new Input();
+
+    public static void setMyInput(Input myInput) {
+
+    }
+}
